dcef57a609ac3052031617497765e6ad95612224
pom-pon <maryvolkovec@gmail.com>
2023-02-24 19:17:01 +0300

    корректировка спрайтов

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def load_image(name, colorkey=None):
 
 
 def main():
-    global FPS_clock, mainSurface, images, TILEMAPPING, OUTSIDEDECOMAPPING, text, PLAYERIMAGES, currentImage
+    global FPS_clock, mainSurface, images, barriers, outside, text, players, currentImage
     pygame.init()
     FPS_clock = pygame.time.Clock()
     sound = pygame.mixer.Sound('music1.wav')
@@ -53,60 +53,28 @@ def main():
     tile_size = tile_width, tile_height = 50, 85
     coin_size = coin_width, coin_height = 50, 50
 
-    IMAGESDICT = {'uncovered goal': pygame.transform.scale(load_image('Bronze_30.png'), coin_size),
-                  'covered goal': pygame.transform.scale(load_image('Silver_21.png'), coin_size),
-                  'star': pygame.transform.scale(load_image('Silver_21.png'), coin_size),
-                  'corner': pygame.transform.scale(load_image('Tile_03.png'), tile_size),
-                  'wall': load_image('crystal_blue2.png'),
-                  'inside floor': load_image('Plain_Block.png'),
-                  'outside floor': pygame.transform.scale(load_image('Grass_Block.png'), tile_size),
-                  'title': load_image('star_title.png'),
-                  'solved': load_image('star_solved.png'),
-                  'princess': load_image('ghost_.png'),
-                  'boy': load_image('ghost_.png'),
-                  'catgirl': load_image('ghost_.png'),
-                  'horngirl': load_image('ghost_.png'),
-                  'pinkgirl': load_image('pinkgirl.png'),
-                  'rock': load_image('Rock.png'),
-                  'short tree': pygame.transform.scale(load_image('bush7_3.png'), tile_size),
-                  'tall tree': pygame.transform.scale(load_image('birch_4.png'), tile_size),
-                  'ugly tree': pygame.transform.scale(load_image('jungle_tree_5.png'), tile_size)}
+    images = {'goal': pygame.transform.scale(load_image('Bronze_30.png'), coin_size),
+              'goal with coin': pygame.transform.scale(load_image('Silver_21.png'), coin_size),
+              'coin': pygame.transform.scale(load_image('Silver_21.png'), coin_size),
+              'corner': pygame.transform.scale(load_image('Tile_03.png'), tile_size),
+              'wall': load_image('crystal_blue2.png'),
+              'inside floor': load_image('Plain_Block.png'),
+              'outside floor': pygame.transform.scale(load_image('Grass_Block.png'), tile_size),
+              'title': load_image('star_title.png'),
+              'solved': load_image('star_solved.png'),
+              'ghost': load_image('ghost_.png'),
+              'boy': load_image('ghost_.png'),
+              'girl': load_image('ghost_.png'),
+              'cat': load_image('ghost_.png'),
+              'pinkgirl': load_image('pinkgirl.png'),
+              'rock': load_image('Rock.png'),
+              'short tree': pygame.transform.scale(load_image('bush7_3.png'), tile_size),
+              'tall tree': pygame.transform.scale(load_image('birch_4.png'), tile_size),
+              'ugly tree': pygame.transform.scale(load_image('jungle_tree_5.png'), tile_size)}
 
-
-    def main():
-        global FPS_clock, mainSurface, images, barriers, outside, text, players, currentImage
-        pygame.init()
-        FPS_clock = pygame.time.Clock()
-        sound = pygame.mixer.Sound('music1.wav')
-        sound.play()
-
-        mainSurface = pygame.display.set_mode((width, height))
-
-        pygame.display.set_caption('Gold Rush')
-        text = pygame.font.Font('arial.ttf', 20)
-
-        images = {'goal': pygame.image.load('.png'),
-                  'goal with coin': pygame.image.load('.png'),
-                  'coin': pygame.image.load('.png'),
-                  'corner': pygame.image.load('.png'),
-                  'wall': pygame.image.load('.png'),
-                  'inside floor': pygame.image.load('.png'),
-                  'outside floor': pygame.image.load('.png'),
-                  'title': pygame.image.load('.png'),
-                  'solved': pygame.image.load('.png'),
-                  'girl': pygame.image.load('.png'),
-                  'boy': pygame.image.load('.png'),
-                  'ghost': pygame.image.load('.png'),
-                  'cat': pygame.image.load(''),
-                  'pinkgirl': pygame.image.load(''),
-                  'rock': pygame.image.load('.png'),
-                  'short tree': pygame.image.load('.png'),
-                  'tall tree': pygame.image.load('.png'),
-                  'ugly tree': pygame.image.load('.png')}
-
-        players = [images['girl'],
+        players = [images['ghost'],
                    images['boy'],
-                   images['ghost'],
+                   images['girl'],
                    images['cat'],
                    images['pinkgirl']]
 
